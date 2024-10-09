f60b69e66d86ae202a33d496d33dbd310bf54937
anorton3 <anorton3@uwyo.edu>
2024-10-09 10:58:14 -0600

    dd

--- ArcGisMap/map.js
+++ ArcGisMap/map.js
@@ -105,7 +105,7 @@ require(
                         
 
                           graphicsLayer.add(pointGraphic);
-                          clusterLayer.add(pointGraphic);
+                         // clusterLayer.add(pointGraphic);
 
                     }
                     
