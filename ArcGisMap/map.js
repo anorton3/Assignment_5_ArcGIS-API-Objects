6abdc75240f6a90060bbbb2d5ce67ddddb480faa
anorton3 <anorton3@uwyo.edu>
2024-10-09 11:20:44 -0600

    content

--- ArcGisMap/map.js
+++ ArcGisMap/map.js
@@ -89,10 +89,9 @@ require(
                           const pointGraphic = new Graphic({
                             geometry: point,
                             symbol: markerSymbol,
-                            attributes: {"Hometown":"North Pole", "Alaska"},
                             popupTemplate: {
-                                title: "North Pole, Alaska -- Hometown",
-                                content: "Lived here for  years"
+                                title: key + ": " + value.city + ", " + value.state,
+                                content: "This is an important point in my life",
                             }
                             
                           });
